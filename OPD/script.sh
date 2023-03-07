e63a6c51a3d3f1b114c85dfea7a5bd37cb221b8c
Aliaksei Kaliada <alex17koleda@gmail.com>
2023-03-07 14:33:02 +0300

    clear after updates

--- OPD/script.sh
+++ OPD/script.sh
@@ -1,18 +1,10 @@
-#git add test.txt
-#git commit -m 'auto upd'
-#git push
-
-
-
 #!/bin/sh
 while true
 do
   echo "Looping"
-  git add test.txt
+  git add черновик.txt
   git commit -m 'auto upd'
   git push
 
-  sleep 5s
+  sleep 10s
 done
-
-
