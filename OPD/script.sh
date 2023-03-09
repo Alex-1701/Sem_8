b92ed36210df9d7ddb199378a264c704552446d9
Aliaksei Kaliada <alex17koleda@gmail.com>
2023-03-09 09:20:13 +0300

    edit script

--- OPD/script.sh
+++ OPD/script.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+
+timestamp() {
+  date +"%T" # current time
+}
+
 while true
 do
-  echo "Looping"
+  echo
+  echo
+  echo "$(timestamp) commit..."
+
   git add черновик.txt
   git commit -m 'auto upd'
   git push
 
-  sleep 10s
+  sleep 1m
 done
